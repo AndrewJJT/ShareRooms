c4017d4564e036a3c404a5617a31fb07f12a2f8a
AndrewJJT <jjtan9886@gmail.com>
2018-12-24 14:26:26 -0800

    Add project files.

---
+++ ShareRooms.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShareRooms.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShareRooms.Domain")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47846791-145f-445d-889f-79143ce7889c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ShareRooms.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShareRooms.WebUI.DAL;
+using ShareRooms.WebUI.Models;
+using System.Data.Entity;
+
+
+namespace ShareRooms.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ShareRoomsContext db = new ShareRoomsContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Country)
+        {
+            
+
+            var result = new ViewModelResult();
+
+            result.Properties = db.Properties
+            //.Include(r =>db.Rooms)
+            .Join(db.Rooms, (p => p.RoomID),
+            (r => r.RoomID), ((p, r) => new { p.Streetaddress, p.State, p.Country, r.SizeinSqFt}))
+            .Where(x => x.p.Country.Contains(Country));
+      
+                        
+            return View("SearchResult", result.Properties.ToList());
+        }
+    }
+}
